207a36a8f8e4f73379990a19cef1d3a1c820dc8b
Sachin Singh <sachinsingh19012@gmail.com>
2025-12-14 23:21:36 +0530

    tuple practice question

---
+++ 31_problem.py
@@ -0,0 +1,4 @@
+'''Write code to print the middle two elements of this tuple:
+t = (10, 20, 30, 40)'''
+t = (10, 20, 30, 40)
+print(t[1:3])
---
+++ 32_problem.py
@@ -0,0 +1,6 @@
+'''Predict the output:
+nums = (1, 2, 3, 4, 5, 6)
+print(nums[::-1])'''
+nums = (1, 2, 3, 4, 5, 6)
+print(nums[::-1])
+# output=(6,5,4,3,2,1)
