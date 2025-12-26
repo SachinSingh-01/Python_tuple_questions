b15a56e82c6a3acc5b7129d061494d2511d08f22
Sachin Singh <sachinsingh19012@gmail.com>
2025-12-26 20:07:25 +0530

    Update 46_problem.py

--- 46_problem.py
+++ 46_problem.py
@@ -1,6 +1,10 @@
 # Write a program to print all elements of a nested tuple using loops:
 # t = (1, (2, 3), (4, 5, 6))
 t = (1, (2, 3), (4, 5, 6))
-c=t[1][0:2]
-d=t[2][0:3]
-print(c+d)
+
+for item in t:
+    if isinstance(item, tuple):
+        for value in item:
+            print(value)
+    else:
+        print(item)
